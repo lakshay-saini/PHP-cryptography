8eca269ba20176de876bde0ab7e78a50cd0883fd
lakshay-saini <39418344+lakshay-saini@users.noreply.github.com>
2019-05-13 15:14:19 +0530

    Update HMACTokenEncryption.php

--- HMACTokenEncryption.php
+++ HMACTokenEncryption.php
@@ -5,7 +5,7 @@
  * Time: 10:11 AM
  */
 
-$data = '1556192278';
+$data = '1557552839709';
 $secret = '6864dfe7-cc72-4342-9a16-fa7f20c8025e-594ac015-2b14-42fe-9f2b-a7b662a6ad23';
 $IVECTOR = 'somerandstr12345';
 
